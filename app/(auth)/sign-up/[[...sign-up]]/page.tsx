0a44ab99c427ee24f61e0ee9cbe1717e5c42ffb8
MikeProLove <michelprosvirnin@gmail.com>
2025-09-14 17:43:12 +0300

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,12 @@
+// app/sign-up/[[...sign-up]]/page.tsx
 'use client';
+
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <main className="p-6">
+      <SignUp />
+    </main>
+  );
 }
