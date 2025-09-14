4eadd76b2f805c6a3423067306a6759cbd4d9dab
MikeProLove <michelprosvirnin@gmail.com>
2025-09-14 17:56:54 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,13 @@
 // middleware.ts
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  // публичные маршруты (доступны без авторизации)
-  publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)'],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // пропускаем статику и служебные файлы
+    // пропускаем статику и внутренние Next.js пути
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // всегда выполнять для API
+    // всегда запускать для API
     '/(api|trpc)(.*)',
   ],
 };
