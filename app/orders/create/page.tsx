114f140c0472e0742e72c96aa4aa28abc0f33427
MikeProLove <michelprosvirnin@gmail.com>
2025-09-15 14:34:38 +0300

    Update page.tsx

--- app/orders/create/page.tsx
+++ app/orders/create/page.tsx
@@ -1,4 +1,6 @@
 // app/orders/create/page.tsx
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 import { listProducts, listServices } from '@/lib/actions/catalog';
 import { createOrder } from '@/lib/actions/orders';
 import OrderForm from './OrderForm'; // клиентский компонент
