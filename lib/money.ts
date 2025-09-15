1b975a37f7cc2865bd51572f06226bd6b687e685
MikeProLove <michelprosvirnin@gmail.com>
2025-09-15 16:37:46 +0300

    Create money.ts

---
+++ lib/money.ts
@@ -0,0 +1,22 @@
+// Простая утилита форматирования цен
+export function money(
+  value: number | string | null | undefined,
+  currency: 'RUB' | 'USD' | 'EUR' = 'RUB',
+  opts: Intl.NumberFormatOptions = {}
+): string {
+  let num =
+    typeof value === 'string'
+      ? Number(value.replace(',', '.'))
+      : typeof value === 'number'
+      ? value
+      : 0;
+
+  if (!Number.isFinite(num)) num = 0;
+
+  return new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency,
+    maximumFractionDigits: 0,
+    ...opts,
+  }).format(num);
+}
